d81e370ed51f9c624f9658430d7622da55a2d2d6
Justo Rivera <justojul23@hotmail.com>
2025-01-22 17:20:11 -0600

    fix typo on db conf

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgres://koyeb-adm:npg_4hPMCc9QxowX@ep-summer-forest-a43yggr1.us-east-1.pg.koyeb.app/koyebdb'
+SQLALCHEMY_DATABASE_URL = 'postgresql://koyeb-adm:npg_4hPMCc9QxowX@ep-summer-forest-a43yggr1.us-east-1.pg.koyeb.app/koyebdb'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
